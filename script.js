4018cd28149e6f9cb26e24ceb9b9acfb40cd91d6
Sittie <sittiemacabago75@gmail.com>
2025-08-21 13:12:00 +0800

    Added attempts counter and improved game logic

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let randomNumber = Math.floor(Math.random() * 10) + 1;
+let attempts = 0;
 
 function checkGuess() {
-  let userGuess = document.getElementById("guess").value;
+  attempts++;
+  document.getElementById("attempts").textContent = "Attempts: " + attempts;
 
+  let userGuess = document.getElementById("guess").value;
   if (userGuess == randomNumber) {
     document.getElementById("result").textContent = "WINNER!";
     document.getElementById("result").className = "winner";
   } else {
-    document.getElementById("result").textContent = "lose! The number was " + randomNumber;
+    document.getElementById("result").textContent =
+      "LOSE! The number was " + randomNumber;
     document.getElementById("result").className = "lose";
   }
+
+ 
+
   document.getElementById("doneBtn").style.display = "none";
   document.getElementById("tryAgainBtn").style.display = "inline-block";
 }
 
 function resetGame() {
-    randomNumber = Math.floor(Math.random() * 10) + 1;
-    document.getElementById("guess").value = "";
-    document.getElementById("result").textContent = "";
-    document.getElementById("result").className = "";
-    document.getElementById("doneBtn").style.display = "inline-block";
-    document.getElementById("tryAgainBtn").style.display = "none";
+  randomNumber = Math.floor(Math.random() * 10) + 1;
+  attempts = 0;
+
+  document.getElementById("guess").value = "";
+  document.getElementById("result").textContent = "";
+  document.getElementById("result").className = "";
+  document.getElementById("attempts").textContent = "Attempts: " + attempts;
+
+  document.getElementById("doneBtn").style.display = "inline-block";
+  document.getElementById("tryAgainBtn").style.display = "none";
 }
-    
